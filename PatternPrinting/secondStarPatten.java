1b456108bbcce733d94f4c32daa661c2bdb84990
Simple-Calculator <dibyanandmishra@gmail.com>
2026-01-02 13:33:25 +0530

    Star Pattern Printing 2nd Question solved

---
+++ PatternPrinting/secondStarPatten.java
@@ -0,0 +1,26 @@
+// Problem Statement: Print the pattern:
+// * * * * *
+// *       *
+// *       *
+// * * * * *
+
+package PatternPrinting;
+
+public class secondStarPatten {
+    public static void main(String[] args) {
+        int rows = 4;
+        int columns = 5;
+
+        for(int i= 0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                if( i==0 || j==0 || i==rows-1 || j==columns-1){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
