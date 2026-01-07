60fad1f74fe2d5d165004f7f91af232c264de4e7
Simple-Calculator <dibyanandmishra@gmail.com>
2026-01-08 02:15:52 +0530

    Problem on Numbers Pattern

---
+++ PatternPrinting/firstNumberPattern.java
@@ -0,0 +1,21 @@
+// Problem Statement: Print the pattern:
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+
+package PatternPrinting;
+
+public class firstNumberPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print((j+1)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ PatternPrinting/forthNumberPattern.java
@@ -0,0 +1,22 @@
+// Problem Statement: Print the pattern:
+// 1 
+// 0 1 
+// 1 0 1 
+// 0 1 0 1 
+// 1 0 1 0 1
+
+package PatternPrinting;
+
+public class forthNumberPattern {
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++){
+                if((j+i)%2 == 0) System.out.print("1 ");
+                else System.out.print("0 ");
+            }
+            System.out.println();
+        }
+    }
+}
